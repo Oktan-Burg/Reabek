4490701da361e4369f88d137bf73305133475849
BKWR Yasharahla <80918992+Oktan-Burg@users.noreply.github.com>
2023-08-26 01:09:01 +0000

    FIX: Redeploy BUG

--- reabek-main/src/app/page.tsx
+++ reabek-main/src/app/page.tsx
@@ -1,2 +1,16 @@
+import Header, { page_loc } from "@/components/header"
+import { Metadata } from "next"
 import Image from "next/image"
+import React from "react"
 export const dynamic = 'force-dynamic'
+export const metadata: Metadata = {
+  title: "Home - Reabek"
+}
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Header location={page_loc.Home}/>
+      
+    </React.Fragment>
+  )
+}
